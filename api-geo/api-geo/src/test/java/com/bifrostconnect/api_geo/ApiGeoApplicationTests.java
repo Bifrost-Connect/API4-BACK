01ae4226ffcafe0a77939d0009a9b3ba252c7fa5
VitorRibeiro09 <vitorsamulmi09@gmail.com>
2026-09-10 20:27:10 -0300

    Build:Arquivo Inicial do Projeto com configurações do Spring Boot

---
+++ api-geo/api-geo/src/test/java/com/bifrostconnect/api_geo/ApiGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bifrostconnect.api_geo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
