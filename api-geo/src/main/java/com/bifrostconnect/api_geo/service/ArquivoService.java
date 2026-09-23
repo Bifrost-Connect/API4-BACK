f8ee562dc6f549eb3e6a8faa077fda17fc81a307
joaosantos13 <jviniciussantos13@gmail.com>
2026-09-22 21:54:29 -0300

    feat (US02): implementa validacoes de erro e testes de hash na US02

--- api-geo/src/main/java/com/bifrostconnect/api_geo/service/ArquivoService.java
+++ api-geo/src/main/java/com/bifrostconnect/api_geo/service/ArquivoService.java
@@ -1,12 +1,9 @@
 package com.bifrostconnect.api_geo.service;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,13 +19,16 @@ public class ArquivoService {
     @Autowired
     private ArquivoOriginalRepository repository;
 
+    @Autowired
+    private HashService hashService;
+
     // Diretório local que simula o Object Storage da Zona Bruta
     private final String STORAGE_DIR = "zona_bruta_storage/";
 
     public ArquivoOriginal processarEArmazenarArquivo(MultipartFile file, Long processoId, Long usuarioUploadId) throws Exception {
         
-        // 1. Gera o Hash SHA-256 do arquivo
-        String hashSha256 = calcularHash(file);
+        // 1. Gera o Hash SHA-256 do arquivo utilizando o serviço especialista (Tarefa 2)
+        String hashSha256 = hashService.calcularHash(file);
 
         // 2. Verifica se o arquivo já foi enviado (evita duplicidade conforme a constraint UNIQUE)
         if (repository.existsByHashSha256(hashSha256)) {
@@ -45,7 +45,7 @@ public class ArquivoService {
         Path caminhoFisico = Paths.get(STORAGE_DIR + System.currentTimeMillis() + "_" + nomeArquivoOriginal);
         Files.write(caminhoFisico, file.getBytes());
 
-        // 4. Monta o objeto para salvar no banco (Oracle)
+        // 4. Monta o objeto para salvar no banco (Regra de Imutabilidade - Tarefa 3)
         ArquivoOriginal registro = new ArquivoOriginal();
         Processo processoRef = new Processo();
         processoRef.setId(processoId);
@@ -66,20 +66,5 @@ public class ArquivoService {
 
         // 5. Salva no banco de dados e retorna os dados
         return repository.save(registro);
-    }
-
-    // Método auxiliar para calcular o SHA-256
-    private String calcularHash(MultipartFile file) throws NoSuchAlgorithmException, IOException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hashBytes = digest.digest(file.getBytes());
-        
-        // Converte os bytes para string hexadecimal
-        StringBuilder hexString = new StringBuilder();
-        for (byte b : hashBytes) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
     }
 }
